0ae88a37d2e337a8191104d85cf02d0ad47f5487
Abdelrahman <abdelrahman.saeed.khater@gmail.com>
2025-05-22 21:48:26 +0300

    f

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,1 +1,5 @@
-print(f"In 2050, you will be {int(input('Enter your current age: ')) + 27} years old.")
+age = int(input("Enter your current age: "))
+
+age2050 = age + 27
+
+print(f"In 2050, you will be {age2050} years old.")
