ab46654d72eee619a0535d846bd71d3e459c786f
Abdelrahman <abdelrahman.saeed.khater@gmail.com>
2025-05-23 15:32:17 +0300

    f

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -25,7 +25,7 @@ def log_queries(func):
 
 @log_queries
 def fetch_all_users(query):
-    conn = sqlite3.connect('AirBnB_Clone.db')
+    conn = sqlite3.connect('users.db')
     cursor = conn.cursor()
     cursor.execute(query)
     results = cursor.fetchall()
