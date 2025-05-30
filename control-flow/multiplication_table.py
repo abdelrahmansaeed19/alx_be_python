64c834fe9b398621c9299bc63e88bddb0872a453
Abdelrahman <abdelrahman.saeed.khater@gmail.com>
2025-05-30 06:19:18 +0300

    f

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,12 @@
+num_error = False
+while not num_error:
+    try:
+        number = int(input("Enter a number to generate its multiplication table: "))
+        if number < 0:
+            raise ValueError
+        num_error = True
+    except ValueError as e:
+        print(f"Invalid input: {e}. Please enter a valid non-negative integer.")
+
+for i in range(1, 11):
+    print(f"{number} * {i} = {number * i}")
