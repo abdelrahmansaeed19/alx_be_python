46caf09bea1164903f032e444441757a67be534a
Abdelrahman <abdelrahman.saeed.khater@gmail.com>
2025-05-30 05:34:53 +0300

    f

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -9,14 +9,12 @@ while NotStringInputError:
     except ValueError as e:
         print(e)
 # This code prompts the user to enter the current weather and provides advice based on the input.
-
-match weather:
-    case "sunny":
-        print("Wear a t-shirt and sunglasses.")
-    case "rainy":
-        print("Don't forget your umbrella and a raincoat.")
-    case "snowy":
-        print("Make sure to wear a warm coat and a scarf.")
-    case _:
-        print("Sorry, I don't have recommendations for this weather.")
+if weather == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif weather == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
 # This code provides advice based on the weather condition entered by the user.
